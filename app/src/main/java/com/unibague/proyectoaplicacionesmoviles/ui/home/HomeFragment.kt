44ff4c5f1005f87e3f92862009a92f4598ebfdc0
GermanCastellanos <2420201006@estudiantesunibague.edu.co>
2025-04-10 22:36:50 -0500

    Initial commit

---
+++ app/src/main/java/com/unibague/proyectoaplicacionesmoviles/ui/home/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.unibague.proyectoaplicacionesmoviles.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.unibague.proyectoaplicacionesmoviles.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.btnLogin.setOnClickListener {
+            val nombre = binding.editTextText.text.toString()
+            val toast = Toast.makeText(context, "Bienvenido"+ " " + nombre, Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
